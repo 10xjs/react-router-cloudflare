a74176369c8c33f8d37952e38ca4465e66836e4f
Neal Granger <neal@nealg.com>
2025-03-10 16:02:10 -0700

    Initial commit

---
+++ app/entry.worker.ts
@@ -0,0 +1,69 @@
+// import { drizzle } from "drizzle-orm/d1";
+import { createRequestHandler, createCookieSessionStorage, unstable_setDevServerHooks } from "react-router";
+// import { DatabaseContext } from "~/database/context";
+// import * as schema from "~/database/schema";
+import { SessionContext } from "~/lib/session";
+
+declare module "react-router" {
+  export interface AppLoadContext {
+    VALUE_FROM_CLOUDFLARE: string;
+		env: Env
+  }
+}
+
+const remixHandler = createRequestHandler(
+  // @ts-expect-error - virtual module provided by React Router at build time
+  () => import("virtual:react-router/server-build"),
+  import.meta.env.MODE
+)
+
+export default {
+  async fetch (request, env) {
+    // const db = drizzle(env.DB, { schema });
+    const sessionStorage = createCookieSessionStorage({
+      cookie: {
+        path: "/",
+        sameSite: "lax",
+        secrets: [env.SESSION_SECRET],
+        secure: request.url.startsWith("https://"),
+      },
+    });
+
+    const session = await sessionStorage.getSession(
+      request.headers.get("Cookie")
+    );
+    const lastSetCookie = await sessionStorage.commitSession(session);
+    
+    // expose env.kv
+    // Object.assign(globalThis, { env });
+
+    if (import.meta.env.DEV) {
+      unstable_setDevServerHooks({
+        getCriticalCss: (env as any).__RPC.__reactRouterGetCriticalCss
+      });
+    }
+
+    const response = await SessionContext.run(session, () =>
+			remixHandler(request, {
+				VALUE_FROM_CLOUDFLARE: "Hello from Cloudflare",
+				env
+			})
+		);
+
+    const setCookie = await sessionStorage.commitSession(session);
+    if (lastSetCookie !== setCookie) {
+      const headers = new Headers(response.headers);
+      headers.append("Set-Cookie", setCookie);
+
+      return new Response(response.body, {
+        cf: response.cf,
+        headers,
+        status: response.status,
+        statusText: response.statusText,
+        webSocket: response.webSocket,
+      });
+    }
+
+    return response;
+  }
+} satisfies ExportedHandler<Env>;
---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import { vitePluginViteNodeMiniflare } from "@hiogawa/vite-node-miniflare";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import {mergeWorkerOptions} from 'miniflare'
+import wrangler from "wrangler";
+
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild ? {
+      input: "./app/worker/index.ts",
+    } : undefined,
+  },
+  ssr: {
+    target: "webworker",
+    noExternal: true,
+    resolve: {
+      conditions: ["workerd", "browser"],
+    },
+    optimizeDeps: {
+      include: [
+        "react",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "react-dom",
+        "react-dom/server",
+        "react-router",
+      ],
+    },
+  },
+  plugins: [
+    // https://github.com/hi-ogawa/vite-plugins/blob/bd88cc5886fe89224fa80a1545e9337d46c66825/packages/vite-node-miniflare/src/server/plugin.ts
+    vitePluginViteNodeMiniflare({
+      entry: "./app/entry.worker.ts",
+      miniflareOptions: (options) => {
+        // https://github.com/hi-ogawa/vite-plugins/blob/3dd50716bbca49ec475d6a6f732518a7643f7e75/packages/vite-node-miniflare/src/plugin.ts#L132
+        const wranglerOptions = wrangler.unstable_getMiniflareWorkerOptions(
+          "./wrangler.json",
+          "development"
+        );
+        delete wranglerOptions.workerOptions.sitePath;
+
+        mergeWorkerOptions(options, wranglerOptions.workerOptions);
+      },
+      // allow framework to extend RPC to implement some features on main Vite process and expose them to Workerd
+      // (e.g. Remix's DevServerHooks)
+      // https://github.com/hi-ogawa/vite-plugins/blob/bd88cc5886fe89224fa80a1545e9337d46c66825/packages/vite-node-miniflare/src/server/plugin.ts#L97
+      customRpc: {
+        // DevServerHook is implemented via custom rpc
+        // https://github.com/remix-run/react-router/blob/796e9ae10f74ba453b738f9d80049885f24caccf/packages/react-router/lib/server-runtime/dev.ts#L10
+        __reactRouterGetCriticalCss: async (...args: any[]) => {
+          return (globalThis as any)["__reactRouterDevServerHooks"].getCriticalCss(
+            ...args,
+          );
+        },
+      },
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+}));
